4ba8d7ed120f3a31626c4352066c191d1b963f71
tunmise <ogunniyitunmise@gmail.com>
2017-11-23 14:54:07 +0100

    Add middleware to protect routes in apiRoute.js

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -7,5 +7,27 @@ router.post('/users', controllers.users.signup);
 
 router.post('/users/login', controllers.users.signin);
 
+router.use((req, res, next) => {
+  const token = req.body.token || req.query.token || req.headers['access-token'];
+  if (token) {
+    jwt.verify(token, process.env.JSON_WEB_TOKEN_SECRETE, (err, decoded) => {
+      if (err) {
+        res.status(401).json({
+          status: 'failed',
+          message: 'Failed to authenticate token',
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).send({
+      status: 'failed',
+      message: 'No access-token provided',
+    });
+  }
+});
+
 
 export default router;
