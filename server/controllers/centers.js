9fb476ac7a5d64ca6ff87c3f7541f5f51d324c41
tunmise <ogunniyitunmise@gmail.com>
2017-11-23 19:45:51 +0100

    Add codes for updating center in center controller

--- server/controllers/centers.js
+++ server/controllers/centers.js
@@ -89,4 +89,86 @@ export default {
         res.status(400).json({ status: 'error', message: err.message });
       });
   },
+
+  update(req, res) {
+    const centerId = req.params.id;
+    const inputData = {};
+    const inputKeys = Object.keys(req.body);
+    for (let i = 0; i < inputKeys.length; i += 1) {
+      if (typeof (inputKeys[i]) === 'string') {
+        inputData[inputKeys[i].toLowerCase().trim()] = req.body[inputKeys[i]].trim();
+      }
+    }
+    const {
+      name,
+      location,
+      details,
+      capacity,
+      type,
+      facilities,
+      price,
+      image,
+    } = inputData;
+    // const validationOutput = validation.update(inputData);
+    // if (validationOutput !== 'success') {
+    //   res.status(400).json({
+    //     status: 'failed',
+    //     message: validationOutput,
+    //   });
+    //   return;
+    // }
+    centers
+      .findOne({
+        where: {
+          id: centerId,
+        },
+      })
+      .then((centerData) => {
+        if (!centerData) {
+          res.status(400).json({ sucess: 'failed', message: 'center does not exist' });
+          return;
+        }
+        if (centerData.userId === req.decoded.id) {
+          centerData
+            .update({
+              name: name || centerData.name,
+              location: location || centerData.location,
+              details: details || centerData.details,
+              capacity: capacity || centerData.capacity,
+              type: type || centerData.type,
+              facilities: facilities ? facilities.split(',') : centerData.facilities,
+              price: price || centerData.price,
+              image: image || centerData.image,
+            })
+            .then((updatedCenter) => {
+              res.status(200).json({
+                status: 'success',
+                message: 'center updated',
+                data: {
+                  id: updatedCenter.id,
+                  name: updatedCenter.name,
+                  location: updatedCenter.location,
+                  details: updatedCenter.details,
+                  capacity: updatedCenter.capacity,
+                  type: updatedCenter.type,
+                  facilities: updatedCenter.facilities,
+                  price: updatedCenter.price,
+                  image: updatedCenter.image,
+                },
+              });
+            })
+            .catch((err) => {
+              res.status(400).json({ status: 'error', message: err.message });
+            });
+        } else {
+          res.status(401).json({
+            sucess: false,
+            message: 'Unauthorised to perform this action',
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(400).json({ status: 'error', message: err.message });
+      });
+  },
 };
