d6c9a51f5f4e0fb7b0a4c11af409e55f077e2005
tunmise <ogunniyitunmise@gmail.com>
2017-11-24 10:12:34 +0100

    Move user validaiton to a middleware file

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+import userValidation from './middlewares/userValidaiton';
 import controllers from './controllers/index';
 
 const router = express.Router();
@@ -12,27 +12,7 @@ router.get('/centers', controllers.centers.getAll);
 
 router.get('/centers/:id', controllers.centers.getOne);
 
-router.use((req, res, next) => {
-  const token = req.body.token || req.query.token || req.headers['access-token'];
-  if (token) {
-    jwt.verify(token, process.env.JSON_WEB_TOKEN_SECRETE, (err, decoded) => {
-      if (err) {
-        res.status(401).json({
-          status: 'failed',
-          message: 'Failed to authenticate token',
-        });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    return res.status(401).send({
-      status: 'failed',
-      message: 'No access-token provided',
-    });
-  }
-});
+router.use(userValidation);
 
 router.post('/events', controllers.events.create);
 
