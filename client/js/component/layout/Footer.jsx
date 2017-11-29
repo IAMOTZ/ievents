afe1a6d895586d354dcf0c13c4903c48f578b339
kensanni <kensanni@gmail.com>
2017-11-29 11:54:43 +0100

    Add component footer

---
+++ client/js/component/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="w-100 mt-auto">
+      <div className="container text-white text-center py-5">
+        <h1>Ievents</h1>
+        <p>Copyright &copy; 2017</p>
+      </div>
+    </footer>
+  );
+};
