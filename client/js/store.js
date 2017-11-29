bdfca35eebab233d112a706feab2f192410af37b
kensanni <kensanni@gmail.com>
2017-11-29 09:07:00 +0100

    Create the store

---
+++ client/js/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import reducer from './reducers';
+
+const middleware = applyMiddleware(thunk, logger());
+
+export default createStore(reducer, middleware);
