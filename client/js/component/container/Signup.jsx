bfa482848a42e8e5c67dda33636d2126d73955f7
kensanni <kensanni@gmail.com>
2017-11-29 15:51:51 +0100

    Modify the way component is imported in Signin container

--- client/js/component/container/Signup.jsx
+++ client/js/component/container/Signup.jsx
@@ -5,7 +5,7 @@ import signupStyles from '../../../sass/signup.scss';
 import TopNavigation from '../layout/TopNavigation.jsx';
 import Footer from '../layout/Footer.jsx';
 
-import createUser from '../../actions/userActions';
+import { createUser } from '../../actions/userActions';
 
 @connect((store) => {
   return  {
