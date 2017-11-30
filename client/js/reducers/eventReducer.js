f084e5327f2791bb52d2500845e39801f7eec5d0
kensanni <kensanni@gmail.com>
2017-11-30 15:48:43 +0100

    Add reducer to handle add event action

---
+++ client/js/reducers/eventReducer.js
@@ -0,0 +1,47 @@
+const initialState = {
+  event: [],
+  status: {
+    fetching: false,
+    fetched: false,
+    error: false,
+  },
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADDDING_EVENT': {
+      return {
+        ...state,
+        status: {
+          ...state.status,
+          fetching: true,
+          fetched: false,
+          error: false
+        },
+      };
+    }
+    case 'ADDING_EVENT_RESOLVED': {
+      const { title, description, date, center } = action.payload.center;
+      const newEvent = { title, description, date, center };
+      return {
+        ...state,
+        event: [...state.event, newEvent],
+        status: {
+          ...state.status,
+          fetching: false,
+          fetched: true,
+        },
+      };
+    }
+    case 'ADDING_EVENT_REJECTED': {
+      return {
+        ...state,
+        status: {
+          ...state.status,
+          fetching: false,
+          error: action.payload,
+        },
+      };
+    }
+  }
+}
