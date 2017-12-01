501b8c56520fb9a072c2cf318c9bb7969b8f5140
kensanni <kensanni@gmail.com>
2017-12-01 08:13:13 +0100

    Add the center and event reducer to the store

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
 import user from './authReducer';
+import centers from './centerReducers';
+import events from './eventReducer';
 
 export default combineReducers({
   user,
+  centers,
+  events,
 });
