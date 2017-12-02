065b5c3a240f51d34558171f5310d4d9ec90f660
kensanni <kensanni@gmail.com>
2017-12-02 20:56:16 +0100

    Add functionality for loggin out

--- client/js/component/common/SideNavigation.jsx
+++ client/js/component/common/SideNavigation.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { clearUser } from '../../actions/authAction';
+
+@connect((store) => {
+  return {
+
+  }
+})
 
 // This is the side navigation that appears when a user is logged in(Large Screen)
-const UserSideNav = (props) => {
-  return (
-    <div class="col-lg-2 fixed-top  d-none d-lg-block" id="navigation-section">
-      <div class="mt-5 text-center">
-        <p class="lead text-white pt-3 text-capitalize" id="userName">{props.userName}</p>
-      </div>
-      <div class="pt-3 navigation-links">
-        <div class="list-group">
-          <Link to='/events' class="list-group-item">
-            <i class="fa fa-user-circle fa-fw" aria-hidden="true"></i>&nbsp; My Events
-          </Link>
-          <Link to="addEvent" class="list-group-item">
-            <i class="fa fa-plus fa-fw" aria-hidden="true"></i>&nbsp; Add Events
-          </Link>
-          <Link to="#" class="list-group-item">
-            <i class="fa fa-bank fa-fw" aria-hidden="true"></i>&nbsp; Centers
-          </Link>
-          <Link to="#" class="list-group-item">
-            <i class="fa fa-power-off fa-fw" aria-hidden="true"></i>&nbsp; Logout
-          </Link>
+class UserSideNav extends React.Component {
+  logout = () => {
+    this.props.dispatch(clearUser());
+  }
+  render() {
+    return (
+      <div class="col-lg-2 fixed-top  d-none d-lg-block" id="navigation-section">
+        <div class="mt-5 text-center">
+          <p class="lead text-white pt-3 text-capitalize" id="userName">{this.props.userName}</p>
         </div>
-      </div>
-    </div >
-  );
-};
+        <div class="pt-3 navigation-links">
+          <div class="list-group">
+            <Link to='/events' class="list-group-item">
+              <i class="fa fa-user-circle fa-fw" aria-hidden="true"></i>&nbsp; My Events
+            </Link>
+            <Link to="addEvent" class="list-group-item">
+              <i class="fa fa-plus fa-fw" aria-hidden="true"></i>&nbsp; Add Events
+            </Link>
+            <Link to="#" class="list-group-item">
+              <i class="fa fa-bank fa-fw" aria-hidden="true"></i>&nbsp; Centers
+            </Link>
+            <Link to="/" class="list-group-itsem" onClick={this.logout}>
+              <i class="fa fa-power-off fa-fw" aria-hidden="true"></i>&nbsp; Logout
+            </Link>
+          </div>
+        </div>
+      </div >
+    );
+  }
+}
 
 // This is the side navigation that appears when an admin is logged in(Large Screen)
 const AdminSideNav = (props) => {
