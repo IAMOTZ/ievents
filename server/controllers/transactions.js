0a0acdf09024e495d11f9d7256a521830e0957a8
tunmise <ogunniyitunmise@gmail.com>
2017-12-09 14:37:00 +0100

    Add controller codes for changing the status of a center

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -17,5 +17,71 @@ export default {
       .then((data) => {
         res.send(data);
       })
+  },
+
+  changeStatus(req, res, next) {
+    if (req.query.decision.toLowerCase() === 'allow') {
+      const transactionsId = req.params.id;
+      transactions
+        .findById(Number(transactionsId))
+        .then((transactionData) => {
+          if (!transactionData) {
+            res.status(400).json({
+              status: 'failed',
+              message: 'the transactions does not exist',
+            })
+          } else {
+            transactionData
+              .update({
+                decision: 'allowed',
+              })
+              .then((newTransactionData) => {
+                res.status(200).json({
+                  status: 'success',
+                  message: 'the transactions is successfully allowed',
+                  transaction: newTransactionData,
+                })
+              })
+          }
+        })
+        .catch((err) => {
+          res.status(400).json({
+            status: 'error',
+            message: err.message
+          })
+        });
+    } else if (req.query.decision.toLowerCase() === 'cancel') {
+      const transactionsId = req.params.id;
+      transactions
+        .findById(Number(transactionsId))
+        .then((transactionData) => {
+          if (!transactionData) {
+            res.status(400).json({
+              status: 'failed',
+              message: 'the transactions does not exist',
+            })
+          } else {
+            transactionData
+              .update({
+                decision: 'canceled',
+              })
+              .then((newTransactionData) => {
+                res.status(200).json({
+                  status: 'success',
+                  message: 'the transactions is successfully canceled',
+                  transaction: newTransactionData,
+                })
+              })
+          }
+        })
+        .catch((err) => {
+          res.status(400).json({
+            status: 'error',
+            message: err.message,
+          })
+        })
+    } else {
+      next();
+    }
   }
 }
