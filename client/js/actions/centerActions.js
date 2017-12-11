3e84fa3d83d6a7764dc0fd96015ebaf402d16247
tunmise <ogunniyitunmise@gmail.com>
2017-12-11 16:27:00 +0100

    Add an action to add center in centerActions

--- client/js/actions/centerActions.js
+++ client/js/actions/centerActions.js
@@ -15,11 +15,34 @@ const getAllCenters = () => {
   }
 }
 
+export const addCenter = (centerDetails, userToken) => {
+  return (dispatch) => {
+    dispatch({ type: 'ADDDING_CENTER' });
+    const config = {
+      headers: {
+        "access-token": userToken,
+      }
+    }
+    axios.post(`${apiBaseUrl}/centers`, centerDetails, config)
+      .then((response) => {
+        dispatch({ type: 'ADDING_CENTER_RESOLVED', payload: response.data, })
+      })
+      .catch((err) => {
+        dispatch({ type: 'ADDING_CENTER_REJECTED', payload: err.response.data, });
+      })
+  }
+}
+
 export const showCenterModal = (centerId) => {
   return {
     type: 'SHOW_CENTER_MODAL',
     payload: centerId,
   }
+}
+
+// This action simply reset the status of the center store to its initial state
+export const clearStatus = () => {
+  return ({ type: 'CLEAR_STATUS', });
 }
 
 export default getAllCenters;
