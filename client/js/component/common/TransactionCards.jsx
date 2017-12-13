c249f57b5958f65f4b5d8c5f3ad978253d7d226f
tunmise <ogunniyitunmise@gmail.com>
2017-12-13 22:23:37 +0100

    Add a modal to show event details in the transactions page

--- client/js/component/common/TransactionCards.jsx
+++ client/js/component/common/TransactionCards.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
 
+class TransactionCards extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      eventModalDetails: null,
+    }
+  }
 
-const TransactionCards = (props) => {
-  return (
-    <div id="transactions" class="mt-lg-0">
-      <div id="accordion" role="tablist">
-        {
-          props.centers.map((center, index) => {
-            return (
-              <div class="card w-lg-75 my-3 bg-white" key={index}>
-                <a href={`#collapse${index}`} data-toggle="collapse" class="text-dark">
-                  <div class="card-header" role="tab">
-                    <h5 class="mb-0">
-                      {center.name}
-                    </h5>
-                  </div>
-                </a>
-                <div id={`collapse${index}`} class="collapse" role="tabpanel" data-parent="#accordion">
-                  <div class="card-body">
-                    <TransactionTable transactions={center.transactions} />
+  showEventModal = (e) => {
+    this.setState({
+      eventModalDetails: {
+        title: e.target.dataset.eventTitle,
+        description: e.target.dataset.eventDescription,
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div id="transactions" class="mt-lg-0">
+        <div id="accordion" role="tablist">
+          {
+            this.props.centers.map((center, index) => {
+              return (
+                <div class="card w-lg-75 my-3 bg-white" key={index}>
+                  <a href={`#collapse${index}`} data-toggle="collapse" class="text-dark">
+                    <div class="card-header" role="tab">
+                      <h5 class="mb-0">
+                        {`${center.name}(${center.transactions.length})`}
+                      </h5>
+                    </div>
+                  </a>
+                  <div id={`collapse${index}`} class="collapse" role="tabpanel" data-parent="#accordion">
+                    <div class="card-body">
+                      <TransactionTable
+                        transactions={center.transactions}
+                        btnAction={this.showEventModal} />
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          })
-        }
+              )
+            })
+          }
+        </div>
+        <EventModal event={this.state.eventModalDetails} />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const TransactionTable = (props) => {
@@ -46,7 +66,15 @@ const TransactionTable = (props) => {
           props.transactions.map((transaction, index) => (
             <tr key={index}>
               <th scope="row" class="d-none d-lg-table-cell">{index + 1}</th>
-              <td>{transaction.event.user.email}</td>
+              <td>{transaction.event.user.email}
+                <a href=""
+                  class="d-block d-lg-inline ml-lg-2"
+                  data-toggle="modal"
+                  data-target="#eventModal"
+                  data-event-title={transaction.event.title}
+                  data-event-description={transaction.event.description}
+                  onClick={props.btnAction}>event details</a>
+              </td>
               <td>{transaction.event.date}</td>
               <td>
                 <button class="btn btn-outline-dark p-1 mb-1 mb-sm-0 mr-2">Allow</button>
@@ -57,7 +85,32 @@ const TransactionTable = (props) => {
         }
       </tbody>
     </table>
-  )
+  );
 }
 
+const EventModal = (props) => {
+  if (!props.event) {
+    return null;
+  } else {
+    return (
+      <div class="modal fade" id="eventModal" tabIndex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                {props.event.title}
+              </h5>
+            </div>
+            <div class="modal-body">
+              {props.event.description}
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 export default TransactionCards;
