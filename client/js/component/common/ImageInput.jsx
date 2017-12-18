dc277ecfe86753958a531a4b70091a391a4f16b2
tunmise <ogunniyitunmise@gmail.com>
2017-12-18 01:31:10 +0100

    Create a component ImageInput to handle image inputing

---
+++ client/js/component/common/ImageInput.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Dropzone from 'react-dropzone';
+
+const ImageInput = (props) => {
+  if (props.previousImage) {
+    // Note that the previous image is a url 
+    // [later] The previous image should also be displayed.
+    return (
+      <Dropzone
+        multiple={false}
+        accept="image/jpeg"
+        onDrop={props.onDrop}>
+        <div>
+          <p>Drop a new image or click to select a new image to upload[.jpeg only] </p>
+        </div>
+      </Dropzone>
+    );
+  } else if (props.newImage) {
+    // Note that the new image is an image file
+    return (
+      <Dropzone
+        multiple={false}
+        accept="image/jpeg"
+        onDrop={props.onDrop}>
+        <a href={props.newImage.preview} target="blank">{props.newImage.name}</a>
+      </Dropzone>
+    );
+  } else {
+    return (
+      <Dropzone
+        multiple={false}
+        accept="image/jpeg"
+        onDrop={props.onDrop}>
+        <p>Drop an image or click to select an image to upload[.jpeg only]</p>
+      </Dropzone>
+    );
+  }
+};
+
+export default ImageInput;
