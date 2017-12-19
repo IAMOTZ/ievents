72aedca7a4136c210d8513113d938a97061c91d0
tunmise <ogunniyitunmise@gmail.com>
2017-12-19 18:21:00 +0100

    Move all the client side routing to a routes file

--- client/js/index.jsx
+++ client/js/index.jsx
@@ -1,51 +1,19 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { Router, Route, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from "./store";
+import Routes from './Routes.jsx';
 
 import styles from '../sass/styles.scss'; 
-import Signup from './component/container/Signup.jsx';
-import Signin from './component/container/Signin.jsx';
-import AddEvent from './component/container/AddEvent.jsx';
-import Events from './component/container/Events.jsx';
-import EditEvent from './component/container/EditEvent.jsx';
-import Centers1 from './component/container/Centers1.jsx';
-import Centers2 from './component/container/Centers2.jsx';
-import AddCenter from './component/container/AddCenter.jsx';
-import EditCenter from './component/container/EditCenter.jsx';
-import Transactions from './component/container/Transactions.jsx';
-import Home from './component/container/index.jsx';
 
-const history = createBrowserHistory();
-const App = () => {
-  return (
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/users' component={Signup} />
-      <Route exact path='/users/login' component={Signin} />
-      <Route exact path='/addEvent' component={AddEvent} />
-      <Route exact path='/editEvent' component={EditEvent} />
-      <Route exact path='/events' component={Events} />
-      <Route exact path='/centers1' component={Centers1} />
-      <Route exact path='/centers2' component={Centers2} />
-      <Route exact path='/addCenter' component={AddCenter} />
-      <Route exact path='/editCenter' component={EditCenter} />
-      <Route exact path='/transactions' component={Transactions} />
-    </Switch>
-  )
-}
-
-const app = document.getElementById('app')
+const app = document.getElementById('app');
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <App />
+    <Router>
+      <Routes />
     </Router>
   </Provider>, app
 );
-
-export default history;
 
