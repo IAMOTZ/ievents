6c35e2f4677cda60e2295465b7e22a3137b99def
tunmise <ogunniyitunmise@gmail.com>
2017-12-23 12:09:38 +0100

    Add a funtion to create super admin when setting up database

--- server/setUpDb.js
+++ server/setUpDb.js
@@ -1,13 +1,42 @@
+import dotEnv from 'dotenv';
 import db from './models/index';
+
+dotEnv.config();
+
+const { users } = db;
 
 db.sequelize.sync()
   .then(() => {
-    console.log('Sequelize successfully synchronized with database');
-    process.exit();
-  })
-  .catch((err) => {
-    console.log({
-      message: 'Error synchronizing sequelize with database',
-      error: err.message,
-    });
+    createSuperAdmin(users)
+      .then(() => {
+        process.exit();
+      });
   });
+
+
+const createSuperAdmin = (userModel) => {
+  return new Promise((resolve, reject) => {
+    userModel
+      .findOne({
+        where: {
+          email: process.env.SUPER_ADMIN_EMAIL
+        }
+      })
+      .then((user) => {
+        if (user) {
+          resolve(null);
+        } else {
+          resolve(
+            userModel
+              .create({
+                name: process.env.SUPER_ADMIN_NAME,
+                email: process.env.SUPER_ADMIN_EMAIL,
+                password: process.env.SUPER_ADMIN_PASSWORD,
+                role: 'superAdmin',
+              })
+          );
+        }
+      });
+  });
+}
+
