2d31ada31f5ac2d4d9b9f66d32f2c817aa7c2d68
tunmise <ogunniyitunmise@gmail.com>
2017-12-25 07:01:02 +0100

    Add the status of an event to its card

--- client/js/component/common/EventCards.jsx
+++ client/js/component/common/EventCards.jsx
@@ -21,11 +21,39 @@ export default (props) => {
             <i class="fa fa-map-marker fa-fw" aria-hidden="true"></i>&nbsp; {getCenterById(props.centers, event.centerId).name}
           </p>
           <span className="text-muted">{event.date}&nbsp;</span>
-          <a href="#" className="btn btn-block bg-dark text-white mt-2" onClick={props.remove} id={event.id}>Done</a>
         </div>
+        <EventStatus status={event.status} />
       </div>
     );
   }));
   return eventCards;
+}
 
+const EventStatus = (props) => {
+  switch (props.status) {
+    case ('allowed'): {
+      return (
+        <div class="card-footer text-success text-center">
+          <i class="fa fa-check"></i>&nbsp;
+          <span>Allowed</span>
+        </div>
+      );
+    }
+    case ('canceled'): {
+      return (
+        <div class="card-footer text-danger text-center">
+          <i class="fa fa-times-circle"></i>&nbsp;
+          <span>Canceled</span>
+        </div>
+      );
+    }
+    default: {
+      return (
+        <div class="card-footer text-warning text-center">
+          <i class="fa fa-question-circle"></i>&nbsp;
+          <span>Pending</span>
+        </div>
+      );
+    }
+  }
 }
