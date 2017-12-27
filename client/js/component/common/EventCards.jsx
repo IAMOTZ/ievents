0d7f586edf11a6c466b9b61cff68443356fb0069
tunmise <ogunniyitunmise@gmail.com>
2017-12-27 15:36:39 +0100

    Change the logic behined rendering event status

--- client/js/component/common/EventCards.jsx
+++ client/js/component/common/EventCards.jsx
@@ -6,21 +6,28 @@ export default (props) => {
   const eventCards = props.events.map((event => {
     return (
       <div className="card text-dark bg-white mb-3 mx-auto" style={{ maxWidth: 20 + 'rem' }} key={event.id}>
-        <div className="card-header h4">
-          {event.title}
-          <Link to="/editEvent" className="ml-4 mr-2">
-            <i className="fa fa-pencil" onClick={props.edit} id={event.id}></i>
-          </Link>
-          <a href="#" className="text-primary">
-            <i className="fa fa-trash" onClick={props.startDelete} id={event.id}></i>
-          </a>
+        <div className="card-header d-flex justify-content-between">
+          <span className={event.status === 'allowed' ? "h4 mb-0" : "h4 mb-0 text-muted"}>{event.title}</span>
+          <span>
+            {
+              event.status === 'allowed' ?
+                <Link to="/editEvent" className="ml-4 mr-2">
+                  <i className="fa fa-pencil" onClick={props.edit} id={event.id}></i>
+                </Link> : null
+            }
+            <a href="#" className="text-primary">
+              <i className="fa fa-trash" onClick={props.startDelete} id={event.id}></i>
+            </a>
+          </span>
         </div>
         <div className="card-body">
-          <p className="card-text">{event.description}</p>
-          <p class="mb-1">
-            <i class="fa fa-map-marker fa-fw" aria-hidden="true"></i>&nbsp; {getCenterById(props.centers, event.centerId).name}
-          </p>
-          <span className="text-muted">{event.date}&nbsp;</span>
+          <div className={event.status === 'allowed' ? "" : "text-muted"}>
+            <p className="card-text">{event.description}</p>
+            <p class="mb-1">
+              <i class="fa fa-map-marker fa-fw" aria-hidden="true"></i>&nbsp; {getCenterById(props.centers, event.centerId).name}
+            </p>
+            <span>{event.date}&nbsp;</span>
+          </div>
         </div>
         <EventStatus status={event.status} />
       </div>
@@ -31,14 +38,6 @@ export default (props) => {
 
 const EventStatus = (props) => {
   switch (props.status) {
-    case ('allowed'): {
-      return (
-        <div class="card-footer text-success text-center">
-          <i class="fa fa-check"></i>&nbsp;
-          <span>Allowed</span>
-        </div>
-      );
-    }
     case ('canceled'): {
       return (
         <div class="card-footer text-danger text-center">
@@ -47,11 +46,19 @@ const EventStatus = (props) => {
         </div>
       );
     }
+    case ('done'): {
+      return (
+        <div class="card-footer text-info text-center">
+          <i class="fa fa-exclamation"></i>&nbsp;
+          <span>Done</span>
+        </div>
+      );
+    }
     default: {
       return (
-        <div class="card-footer text-warning text-center">
-          <i class="fa fa-question-circle"></i>&nbsp;
-          <span>Pending</span>
+        <div class="card-footer text-success text-center">
+          <i class="fa fa-check"></i>&nbsp;
+          <span>Allowed</span>
         </div>
       );
     }
