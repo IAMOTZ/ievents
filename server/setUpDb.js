1c5a18d2134b8b1efe5e1f4d14fa91f513a6538d
tunmise <ogunniyitunmise@gmail.com>
2017-12-27 21:53:36 +0100

    Move the createSuperAdmin function to the helpers

--- server/setUpDb.js
+++ server/setUpDb.js
@@ -1,7 +1,5 @@
-import dotEnv from 'dotenv';
 import db from './models/index';
-
-dotEnv.config();
+import { createSuperAdmin } from './helpers';
 
 const { users } = db;
 
@@ -12,31 +10,3 @@ db.sequelize.sync()
         process.exit();
       });
   });
-
-
-const createSuperAdmin = (userModel) => {
-  return new Promise((resolve, reject) => {
-    userModel
-      .findOne({
-        where: {
-          email: process.env.SUPER_ADMIN_EMAIL
-        }
-      })
-      .then((user) => {
-        if (user) {
-          resolve(null);
-        } else {
-          resolve(
-            userModel
-              .create({
-                name: process.env.SUPER_ADMIN_NAME,
-                email: process.env.SUPER_ADMIN_EMAIL,
-                password: process.env.SUPER_ADMIN_PASSWORD,
-                role: 'superAdmin',
-              })
-          );
-        }
-      });
-  });
-}
-
