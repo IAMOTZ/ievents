7c50684311861e177d8aaa67a11a568328aa2013
tunmise <ogunniyitunmise@gmail.com>
2017-12-27 21:52:49 +0100

    Add an helper to create the super admin

--- server/helpers.js
+++ server/helpers.js
@@ -1,8 +1,11 @@
 import sequelize from 'sequelize';
+import dotEnv from 'dotenv';
+
+dotEnv.config();
 
 const Op = sequelize.Op;
 
-const updateEventStatus = (eventModel) => {
+export const updateEventStatus = (eventModel) => {
   const currentDate = getCurrentDate(1); // Get the current time in Nigeria;
   return new Promise((resolve, reject) => {
     eventModel
@@ -37,6 +40,33 @@ const updateEventStatus = (eventModel) => {
   });
 }
 
+export const createSuperAdmin = (userModel) => {
+  return new Promise((resolve, reject) => {
+    userModel
+      .findOne({
+        where: {
+          email: process.env.SUPER_ADMIN_EMAIL
+        }
+      })
+      .then((user) => {
+        if (user) {
+          resolve(null);
+        } else {
+          resolve(
+            userModel
+              .create({
+                name: process.env.SUPER_ADMIN_NAME,
+                email: process.env.SUPER_ADMIN_EMAIL,
+                password: process.env.SUPER_ADMIN_PASSWORD,
+                role: 'superAdmin',
+              })
+          );
+        }
+      });
+  });
+}
+
+
 // Add the extra time to the comparison
 const compareDate = (date1, date2, offset) => {
   if (date1.getTime() > date2.getTime() + offset) {
@@ -56,5 +86,3 @@ export const getCurrentDate = (timeZoneOffset) => {
   ).getTime();
   return new Date(UTCTime + (timeZoneOffset * 60 * 60 * 1000));
 }
-
-export default updateEventStatus;
