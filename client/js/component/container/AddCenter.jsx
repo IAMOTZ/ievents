b24cbec1a68b181f8d459f01ce60ded6948855be
tunmise <ogunniyitunmise@gmail.com>
2017-12-29 20:37:54 +0100

    Restructure the form content in AddCenter container

--- client/js/component/container/AddCenter.jsx
+++ client/js/component/container/AddCenter.jsx
@@ -101,69 +101,98 @@ export default class AddCenter extends React.Component {
                 {/* Content Header(navigation) on large screen */}
                 <Header text='Add a center' />
                 {/* Input form */}
-                <form class="mt-lg-5 mb-md-5 w-lg-50">
+                <form class="mt-lg-5 mb-md-5">
                   <LoadingIcon start={this.props.status.adding} size={2} />
-                  <WarningAlert message={this.props.status.error} />
-                  <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="email"
-                      className="form-control"
-                      id="name"
-                      name="name"
-                      placeholder="The name of the center"
-                      onChange={this.getInput} />
-                    <small class="form-text text-muted">Less than 30 characters</small>
+                  <div className="w-lg-50 mx-auto">
+                    <WarningAlert message={this.props.status.error} />
                   </div>
-                  <div class="form-group">
-                    <label for="location">Location</label>
-                    <input type="text"
-                      class="form-control"
-                      id="location"
-                      name="location"
-                      placeholder="The location of the center"
-                      onChange={this.getInput} />
-                    <small class="form-text text-muted">Less than 30 characters</small>
-                  </div>
-                  <div class="form-group">
-                    <label for="description">Details</label>
-                    <textarea
-                      class="form-control"
-                      id="description" rows="7"
-                      name="details"
-                      placeholder="More details about the center"
-                      onChange={this.getInput}></textarea>
-                    <small class="form-text text-muted">Less than 300 characters</small>
-                  </div>
-                  <div class="form-group">
-                    <label for="capacity">Capacity</label>
-                    <input type="number"
-                      class="form-control"
-                      id="capacity"
-                      name="capacity"
-                      placeholder="How many seats"
-                      onChange={this.getInput} />
-                  </div>
-                  <div class="form-group">
-                    <label for="price">Price</label>
-                    <input type="number"
-                      class="form-control"
-                      id="price"
-                      name="price"
-                      placeholder="Price"
-                      onChange={this.getInput} />
-                  </div>
-                  <div class="form-group">
-                    <label for="image">Image</label>
-                    <div className="text-center">
-                      <ImageInput
-                        id="image"
-                        onDrop={this.handleImageDrop}
-                        newImage={this.state.images ? this.state.images[0] : null}
-                      />
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-12 col-lg-6">
+                        <div class="form-group">
+                          <label for="name">Name</label>
+                          <input type="email"
+                            className="form-control"
+                            id="name"
+                            name="name"
+                            placeholder="The name of the center"
+                            onChange={this.getInput} />
+                          <small class="form-text text-muted">Less than 30 characters</small>
+                        </div>
+                      </div>
+                      <div className="col-12 col-lg-6">
+                        <div class="form-group">
+                          <label for="location">Location</label>
+                          <input type="text"
+                            class="form-control"
+                            id="location"
+                            name="location"
+                            placeholder="The location of the center"
+                            onChange={this.getInput} />
+                          <small class="form-text text-muted">Less than 30 characters</small>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div class="ml-3 pt-3">
-                    <button class="btn btn-outline-dark" disabled={this.props.status.adding} onClick={this.add}>Add</button>
+                    <div className="row">
+                      <div className="col-12 col-lg-6">
+                        <div class="form-group">
+                          <label for="description">Details</label>
+                          <textarea
+                            class="form-control"
+                            id="description" rows="7"
+                            name="details"
+                            placeholder="More details about the center"
+                            onChange={this.getInput}></textarea>
+                          <small class="form-text text-muted">Less than 300 characters</small>
+                        </div>
+                      </div>
+                      <div className="col-12 col-lg-6">
+                        <div className="row">
+                          <div className="col-12">
+                            <div class="form-group">
+                              <label for="capacity">Capacity</label>
+                              <input type="number"
+                                class="form-control"
+                                id="capacity"
+                                name="capacity"
+                                placeholder="How many seats"
+                                onChange={this.getInput} />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-12">
+                            <div class="form-group">
+                              <label for="price">Price</label>
+                              <input type="number"
+                                class="form-control"
+                                id="price"
+                                name="price"
+                                placeholder="Price"
+                                onChange={this.getInput} />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-12">
+                            <div class="form-group">
+                              <label for="image">Image</label>
+                              <div className="text-center">
+                                <ImageInput
+                                  style={{ height: '100px' }}
+                                  id="image"
+                                  onDrop={this.handleImageDrop}
+                                  newImage={this.state.images ? this.state.images[0] : null}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div class="ml-3 pt-3">
+                            <button class="btn btn-outline-dark" disabled={this.props.status.adding} onClick={this.add}>Add</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </form>
               </div>
