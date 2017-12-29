6da42129d85f233a250309fba0ca92e35413c532
tunmise <ogunniyitunmise@gmail.com>
2017-12-29 11:05:32 +0100

    Redesign the warning alert component

--- client/js/component/common/Alert/WarningAlert.jsx
+++ client/js/component/common/Alert/WarningAlert.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 export default (props) => {
   if (props.message) {
     return (
-      <div className="alert alert-warning" role="alert">
-        <strong className="text-black">{props.message}</strong>
+      <div className="bg-danger text-white text-center p-1 rounded">
+        <i class="fa fa-times"></i>&nbsp;
+        <span>{props.message}</span>
       </div>
     )
   }
