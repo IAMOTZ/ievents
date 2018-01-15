fabc44a157d1346107be26c61d391fc8d717d7d1
tunmise <ogunniyitunmise@gmail.com>
2018-01-15 13:43:01 +0100

    Properly export the formatter middleware

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,5 +1,7 @@
+import formatInputDatas from './helpers';
+
 export * from './userInfoValidation';
 export * from './eventInfoValidation';
 export * from './centerInfoValidation';
 export * from './userStatusValidation';
-export * from './helpers';
+export { formatInputDatas };
