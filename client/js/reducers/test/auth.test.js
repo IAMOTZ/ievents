4c0055e66efd8b9704ac9091f0c82c7499bf215e
tunmise <ogunniyitunmise@gmail.com>
2018-01-25 15:18:38 +0100

    Add tests for authentication reducer

---
+++ client/js/reducers/test/auth.test.js
@@ -0,0 +1,130 @@
+import reducer from '../authReducer';
+import * as actionTypes from '../../actions/actionTypes';
+
+let previousUser;
+const sampleUser = {
+  name: 'test',
+  email: 'test@gmail.com',
+  role: 'user',
+  id: 1,
+};
+const token = 'justATestToken';
+
+const initialState = {
+  user: previousUser || {
+    name: null,
+    email: null,
+    role: null,
+    id: null,
+    token: null,
+  },
+  status: {
+    fetching: false,
+    fetched: Boolean(previousUser),
+    error: false,
+    addingAdmin: false,
+    adminAdded: false,
+    addingAdminError: false,
+  },
+};
+const alterInitialState = (newUser, newStatus) => ({
+  user: { ...initialState.user, ...newUser },
+  status: { ...initialState.status, ...newStatus },
+});
+
+const errorMessage = 'there was an error';
+const TOKEN_NAME = 'IEVENTS_USER_TOKEN';
+
+describe('Auth Reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  describe('Signing up a user', () => {
+    it('should update the fetching status to true', () => {
+      expect(reducer(undefined, {
+        type: actionTypes.ADDING_USER,
+      })).toEqual(alterInitialState({}, { fetching: true }));
+    });
+    it('should update the user info and store a token', () => {
+      expect(reducer(undefined, {
+        type: actionTypes.ADDING_USER_RESOLVED,
+        payload: { user: sampleUser, token },
+      })).toEqual(alterInitialState({ ...sampleUser, token }, { fetched: true }));
+      expect(localStorage.getItem(TOKEN_NAME)).toEqual(token);
+    });
+    it('should update the error status with some error message', () => {
+      expect(reducer(undefined, {
+        type: actionTypes.ADDING_USER_REJECTED,
+        payload: errorMessage,
+      })).toEqual(alterInitialState({}, { error: errorMessage }));
+    });
+  });
+
+  describe('Logging in a user', () => {
+    it('should update the fetching status to true', () => {
+      expect(reducer(undefined, {
+        type: actionTypes.LOGGING_USER,
+      })).toEqual(alterInitialState({}, { fetching: true }));
+    });
+    it('should update the user info and store a token', () => {
+      expect(reducer(undefined, {
+        type: actionTypes.LOGGING_USER_RESOLVED,
+        payload: { user: sampleUser, token },
+      })).toEqual(alterInitialState({ ...sampleUser, token }, { fetched: true }));
+      expect(localStorage.getItem(TOKEN_NAME)).toEqual(token);
+    });
+    it('should update the error status with some error message', () => {
+      expect(reducer(undefined, {
+        type: actionTypes.LOGGING_USER_REJECTED,
+        payload: errorMessage,
+      })).toEqual(alterInitialState({}, { error: errorMessage }));
+    });
+  });
+
+  describe('Signing up an Admin', () => {
+    it('should update the addingAdmin status to true', () => {
+      expect(reducer(undefined, {
+        type: actionTypes.ADDING_ADMIN,
+      })).toEqual(alterInitialState({}, { addingAdmin: true }));
+    });
+    it('should update adminAdded status to true', () => {
+      expect(reducer(undefined, {
+        type: actionTypes.ADDING_ADMIN_RESOLVED,
+      })).toEqual(alterInitialState({}, { adminAdded: true }));
+    });
+    it('should update the addingAdminError status with some error message', () => {
+      expect(reducer(undefined, {
+        type: actionTypes.ADDING_ADMIN_REJECTED,
+        payload: errorMessage,
+      })).toEqual(alterInitialState({}, { addingAdminError: errorMessage }));
+    });
+  });
+
+  describe('Clearing all or part of the state', () => {
+    it('should clear all the error status', () => {
+      expect(reducer(alterInitialState({}, { fetching: true, error: errorMessage }), {
+        type: actionTypes.CLEAR_USER_STATUS,
+        payload: 'ERROR',
+      })).toEqual(alterInitialState({}, { fetching: true }));
+    });
+    it('should clear all the admin related status', () => {
+      expect(reducer(alterInitialState({}, { fetching: true, adminAdded: true }), {
+        type: actionTypes.CLEAR_USER_STATUS,
+        payload: 'ADD_ADMIN',
+      })).toEqual(alterInitialState({}, { fetching: true }));
+    });
+    it('should not clear any status', () => {
+      expect(reducer(alterInitialState({}, { fetching: true }), {
+        type: actionTypes.CLEAR_USER_STATUS,
+        payload: '',
+      })).toEqual(alterInitialState({}, { fetching: true }));
+    });
+    it('should reinitialize the user state', () => {
+      expect(reducer(alterInitialState({ ...sampleUser, token }, { fetched: true }), {
+        type: actionTypes.CLEAR_USER,
+      })).toEqual(initialState);
+      expect(localStorage.getItem(TOKEN_NAME)).toEqual(null);
+    });
+  });
+});
